ab352487f82151606b9e34ae717f971ed82eaf40
UP819950 <up819950@myport.ac.uk>
2020-10-14 15:52:41 +0100

    completed week1 ex 2

--- Exercises/src/week1Ex2.java
+++ Exercises/src/week1Ex2.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class week1Ex2 {
-}
+
+
+
+public class week1Ex2 {
+
+    public static void main(String[] args) {
+
+    int i = 0;
+        while (i < 21){
+
+            int y = (-3 * i ) - 2 ;
+            System.out.println(i + ", " + y);
+            i ++ ;
+        }
+        }
+    }
+
