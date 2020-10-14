bc5d99b463d79a78e1db31411298cbbff2bb0257
UP819950 <up819950@myport.ac.uk>
2020-10-14 15:44:02 +0100

    Completed task 1

--- Exercises/src/exercise01.java
+++ Exercises/src/exercise01.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class exercise01 {
+public class exercise01 {
+
+    /*Ex1*/
+
+    public static void main(String[] args) {
+
+        int x = 0 ;
+
+        for (int i = 0 ; i < 21; i ++ ){
+
+            int y = (-3 * i) - 2 ;
+
+            System.out.println(x + ", " + y);
+
+            x++;
+        }
+
+    }
+
+
+
+
 }
